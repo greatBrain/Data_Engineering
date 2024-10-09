d3e6f0e7426d1e54843655a42847fdfd1984c7fd
greatbrain <erickfania@gmail.com>
2024-10-09 17:38:38 -0400

    .

---
+++ exercices/exercise_1_download_files/main.py
@@ -0,0 +1,22 @@
+import requests
+import os
+import sys
+
+download_uris = [    
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip",
+]
+
+def download()-> None:
+    pass
+
+
+
+def run():
+    pass
+
+
+if __name__ == __main__:
+    run() 
