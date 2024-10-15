4f7446694871d60741b69d8afe7c38c04222b58a
greatbrain <erickfania@gmail.com>
2024-10-15 07:22:44 -0400

    refactor:turn to object oriented

--- exercices/exercise_1_download_files/main.py
+++ exercices/exercise_1_download_files/main.py
@@ -2,68 +2,73 @@ import requests as rq
 import os
 from zipfile import ZipFile
 
-download_uris = [   
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
-]
+class Downloader:
+      def __init__(self):
+          self.download_uris = [
+              "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
+              "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
+              "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip",
+              "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip",
+              "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip",
+              "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
+          ]
 
-def get_dir() -> str:
-    down_dir = 'download'
+          self.down_dir = 'download'
 
-    if not os.path.exists(down_dir):
-       os.makedirs(down_dir)      
-    return down_dir
+      def get_dir(self) -> str:
+          self.down_dir
 
-def download()-> None:
-    dir = get_dir()
-
-    while True:
-        for item in range(len(download_uris)):
-            result = rq.get(download_uris[item], allow_redirects=True)
-            file_name = download_uris[item].split('/')[-1]
-            open('{}/{}'.format(dir, file_name), 'wb').write(result.content)
-        break
-
-    print("File downloading ready...", "\n", "Unzipping files:")
-    unzip_files_in_dir(dir)
+          if not os.path.exists(self.down_dir):
+             os.makedirs(self.down_dir)
+          return self.down_dir
 
 
-def unzip_files_in_dir(dir:str) -> None:   
-    zip_files = [file for file in os.listdir(dir) if file.endswith('.zip')]
+      def download(self) -> None:
+          dir = self.get_dir()
 
-    if not zip_files:
-       print("No .zip files for unzip...") 
+          while True:
+              for item in range(len(self.download_uris)):
+                  result = rq.get(self.download_uris[item], allow_redirects=True)
+                  file_name = self.download_uris[item].split('/')[-1]
+                  open('{}/{}'.format(dir, file_name), 'wb').write(result.content)
+              break
 
-    for file in zip_files:
-        file_path = os.path.join(dir, file)
-
-        try:
-            with ZipFile(file_path, "r") as zip_file_rpr:
-                 print("Extracting.... Please wait.")
-                 zip_file_rpr.extractall(dir)
-                 delete_zip_files(dir)
-            print("files extracted sucessfully!")
-
-        except Exception as e:
-            print("Error unzipping {}, {}".format(file, e))
+          print("File downloading ready...", "\n", "Unzipping files:")
+          self.unzip_files_in_dir(dir)
 
 
-def delete_zip_files(dir:str):
-    files = os.listdir(dir)
+      def unzip_files_in_dir(self, dir:str) -> None:
+          zip_files = [file for file in os.listdir(dir) if file.endswith('.zip')]
 
-    for file in files:
-        if not file.endswith('.csv'):
-           file_path = os.path.join(dir, file)
-           os.remove(file_path)
-    print("zip files removed!")
+          if not zip_files:
+             print("No .zip files for unzip...")
+
+          for file in zip_files:
+              file_path = os.path.join(dir, file)
+
+              try:
+                  with ZipFile(file_path, "r") as zip_file_rpr:
+                       print("Extracting.... Please wait.")
+                       zip_file_rpr.extractall(dir)
+                       self.delete_zip_files(dir)
+                  print("files extracted sucessfully!")
+              except Exception as e:
+                  print("Error unzipping {}, {}".format(file, e))
 
 
-def run() -> None:
-    download()
+      def delete_zip_files(self, dir:str):
+           files = os.listdir(dir)
+
+           for file in files:
+               if not file.endswith('.csv'):
+                  file_path = os.path.join(dir, file)
+                  os.remove(file_path)
+           print("zip files removed!")
+
+
+      def run(self) -> None:
+          self.download()
 
 if __name__ == "__main__":
-    run() 
+   downloader = Downloader()
+   downloader.run()
