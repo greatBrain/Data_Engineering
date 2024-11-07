285ba7566da36bcb75a28d204f799243b5da73c9
greatbrain <erickfania@gmail.com>
2024-11-07 01:38:51 -0400

    fix:csv files saving in csv dir.

--- exercices/exercise_3_Convert_JSON_to_CSV_Ragged_Directories/main.py
+++ exercices/exercise_3_Convert_JSON_to_CSV_Ragged_Directories/main.py
@@ -11,28 +11,38 @@ def convert_to_csv(file) -> None:
         j_data = json.load(f)
 
     # Verify the json is a list of dicts, common when converting to CSV
-    if isinstance(j_data, list) and all(isinstance(item, dict) for item in j_data):
+    if isinstance(j_data, list) and all(isinstance(item, dict) for item in j_data):  
+       
+       csv_files_dir = create_csv_dir()
 
-       # Defining the csv file name:
-       csv_file = os.path.splitext(file)[0] + '.csv'
+       csv_file_base_name = os.path.splitext(os.path.basename(json_file))[0]
+       csv_file = os.path.join(csv_files_dir, "{}.csv".format(csv_file_base_name))
 
        # Write to csv
-       with open(csv_file, 'w+', newline='') as f:
+       with open(csv_file, 'w+', newline='', encoding='utf-8') as f:
             writer = csv.DictWriter(f, fieldnames=j_data[0].keys())
             writer.writeheader()
-            writer.writerows(j_data)
-            
-            print("Json file converted to csv. {}".format(csv_file))
+            writer.writerows(j_data)            
+            print("Json file converted to csv. {}. CSV files saved on new directory.".format(csv_file))           
 
     else:
        print("Unspected format in {}, convertion was not possible.".format(file))
 
 
-# Finding json files inside any directory:
-def get_json_files()->list:
-    json_files = glob.glob('**/*.json', recursive=True)
-    return json_files
-  
+# AUX function:
+def create_csv_dir()-> None:
+    csv_files_dir = 'csv_files_dir'
 
-for json_file in get_json_files():
+    if not os.path.exists(csv_files_dir):
+        os.makedirs(csv_files_dir)    
+    return csv_files_dir
+
+
+# Finding json files inside any directory:
+def _get_json_files()->list:
+    json_files = glob.glob('**/*.json', recursive=True)
+    return json_files  
+
+
+for json_file in _get_json_files():
     convert_to_csv(json_file)
